03c182182db99eed4ce38b4f9377f33f51312e7e
Neyolim <neyoollim@gmail.com>
2025-09-03 15:50:31 +0545

    update Readme file

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = "mongodb+srv://neyongl33:w5kAxQNq4RU4a1Bl@book-store.t1vhdj6.mongodb.net/books-collection?retryWrites=true&w=majority&appName=book-store"
+// write your mongoURI String
